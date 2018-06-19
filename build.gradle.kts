4a2ced5c8a35d1a1a9d4bbb6686af0cb9f55d611
Rick Slot <rick.slot94@gmail.com>
2018-06-19 08:39:02 +0200

    update logging dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencyManagement {
 }
 
 dependencies {
-	implementation("org.springframework.fu.module:spring-fu-logging")
+	implementation("org.springframework.fu.module:spring-fu-logging-logback")
 	implementation("org.springframework.fu.module:spring-fu-webflux-netty")
 	implementation("org.springframework.fu.module:spring-fu-jackson")
 	implementation("org.springframework.fu.module:spring-fu-mongodb")
